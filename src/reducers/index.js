95f0a4e4b6c5c4bbc4352b1bb9ca5c48fc3fb749
Georgi Tsanev <cologneto@gmail.com>
2019-02-14 09:58:10 +0200

    React starting

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import books from "./books_reducer";
+import user from "./user_reducer"
+
+
+const rootReduser = combineReducers({
+    books,
+    user
+})
+
+export default rootReduser;
