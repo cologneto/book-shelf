34d18b942edf081213418ccd302913c05e10bdfa
Georgi Tsanev <cologneto@gmail.com>
2019-02-14 10:37:57 +0200

    React starting

---
+++ src/components/Header/header.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import FontAwesome from 'react-fontawesome';
+import {Link} from "react-router-dom";
+
+class Header extends Component {
+
+    state = {
+        showNav: false,
+    }
+
+    render() {
+        return (
+            <header>
+                <div className="open_nav">
+                    <FontAwesome name="bars"
+                         style={{
+                             color: '#ffffff',
+                             padding: '10px',
+                             cursor: "pointer"
+
+                         }}
+                    />
+
+                </div>
+                <Link to="/" className="logo">
+                    The Book Shelf
+                </Link>
+            </header>
+        );
+    }
+}
+
+Header.propTypes = {};
+
+export default Header;
